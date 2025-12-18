a40eae40f06d17db703ea5b6dbefaa2b8bb9a4ef
Reda Hilal <reda0x04@gmail.com>
2025-12-18 04:02:46 +0400

    implmenting login endpoints

---
+++ server/Tafa3ul.API/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Tafa3ul.API.Entities;
+using Tafa3ul.API.Models;
+
+namespace Tafa3ul.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto dto)
+        {
+            var hashPassword = new PasswordHasher<User>().HashPassword(user, dto.Password);
+
+            user.Username = dto.Username;
+            user.PasswordHash = hashPassword;
+            return Ok(user); 
+        }
+
+        [HttpPost("login")]
+        public ActionResult Login(UserDto userDto)
+        {
+            if (user.Username != userDto.Username)
+                return Unauthorized("Invalid username");
+
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, userDto.Password) == PasswordVerificationResult.Failed)
+                return Unauthorized("Invalid password");
+
+            string token = "success";
+            return Ok(token);
+        }
+    }
+}
---
+++ server/Tafa3ul.API/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Tafa3ul.API.Entities
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+
+
+    }
+}
