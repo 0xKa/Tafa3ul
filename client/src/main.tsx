ba47baed695b5b9b9bd60a8bf367dbd8e4d32c9a
Reda Hilal <reda0x04@gmail.com>
2026-01-12 11:16:06 +0400

    add state libs and setup tanstack query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
-import { router } from "./router.tsx";
-import "./index.css";
 import { ThemeProvider } from "./components/theme/theme-provider.tsx";
+import "./index.css";
+import { router } from "./router.tsx";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
 );
